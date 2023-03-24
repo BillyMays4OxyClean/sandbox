164ced95ba27ead1db19e2a78d08e87ceb12d7fd
Luke Patterson <luke.patterson@icloud.com>
2023-03-23 20:11:16 -0500

    Adding project consts for definitions that will be added later. Modified main.cpp

--- Scripts/newproject/src/main.cpp
+++ Scripts/newproject/src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <fstream>
+using namespace std;
 
 int main(int argc,char * argv[])
 {
-	std::cout << "Hi mom" << std::endl;
+	if (argc == 1)
+	{
+		ofstream myfile;
+		myfile.open("CMakeLists.txt");
+		myfile << "cmake_minimum_required(VERSION 3.1)
+	}
 }
