c768b6dfba8a9c7c4f05fe3c61f5964ba6085b95
Luke Patterson <lcp.patterson@gmail.com>
2023-04-11 22:47:08 -0500

    More arg testing

--- Scripts/ArgTesting/src/main.cpp
+++ Scripts/ArgTesting/src/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main(int argc,char * argv[])
 {
-	std::cout << argc << std::endl;
+	cout << argc << endl;
 	for (int i = 0; i < argc; i++)
-		std::cout << i << "\t:\t" << argv[i] << std::endl;
+		cout << i << "\t:\t" << argv[i] << endl;
+
+	cout << string(argv[1]).at(0) << endl;
+	cout << string(argv[1]).at(1) << endl;
+	cout << string(argv[1]).substr(2,3) << endl;
 }
